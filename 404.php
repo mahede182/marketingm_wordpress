0ea3378b701d1153b06288a92974395642a2449d
mahede182 <53116487+mahede182@users.noreply.github.com>
2021-01-30 21:58:54 +0600

    if any query not found then show it

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header();?>
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>Oops!</h1>
+				<h2>404 - The Page can't be found</h2>
+			</div>
+			<a href="<?php echo get_home_url(); ?>">Go TO Homepage</a>
+		</div>
+	</div>
+<?php get_footer();?>
